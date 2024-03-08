d42080e982b8cd2406bbd3ec68976975ccab62ba
t0mri <dat0mri@proton.me>
2024-03-08 13:13:34 +0530

    feat: add sub_contracts table

---
+++ database/migrations/2024_03_08_074232_create_sub_contracts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_contracts', function (Blueprint $table) {
+                        $table->uuid("id")->primary();
+            $table->foreignUuid("contractor_id")->constrained();
+            $table->foreignUuid("project_id")->constrained()->onDelete("cascade");
+            $table->string("work");
+            $table->date("start_date");
+            $table->date("completion_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_contracts');
+    }
+};
