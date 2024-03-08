8205de265a07b2b09f2625268691c5cb516b5f30
t0mri <dat0mri@proton.me>
2024-03-08 13:09:18 +0530

    feat: add client_payments table

---
+++ database/migrations/2024_03_08_073813_create_client_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_payments', function (Blueprint $table) {
+                        $table->uuid("id")->primary();
+            $table->foreignUuid("client_id")->constrained();
+            $table->foreignUuid("project_id")->constrained()->onDelete("cascade");
+            $table->foreignUuid("transaction_id")->constrained()->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_payments');
+    }
+};
