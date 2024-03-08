380093edba799289b3a36d8a9f82b341c0003b75
t0mri <dat0mri@proton.me>
2024-03-08 13:34:25 +0530

    feat: add sub_contracts relations

--- app/Models/SubContract.php
+++ app/Models/SubContract.php
@@ -9,4 +9,19 @@ use Illuminate\Database\Eloquent\Model;
 class SubContract extends Model
 {
     use HasFactory, HasUuids;
+
+        public function contractor()
+    {
+        return $this->belongsTo(Contractor::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
